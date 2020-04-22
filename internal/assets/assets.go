c6b1c052b74fb52ae317a2be071b2d429263ca8f
Carlo Field <carlo@swiss.dev>
2020-04-22 14:27:45 +0200

    initial commit

---
+++ internal/assets/assets.go
@@ -0,0 +1,87 @@
+package assets
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"sync"
+)
+
+var DefaultAssetLoader AssetsLoader
+
+type AssetsLoader interface {
+	GetDB() PartsDatabase
+	GetBtik() map[string]string
+}
+
+func GetDB() PartsDatabase {
+	return DefaultAssetLoader.GetDB()
+}
+
+func GetBtik() map[string]string {
+	return DefaultAssetLoader.GetBtik()
+}
+
+func init() {
+	p, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	DefaultAssetLoader = StaticFileAssetLoader{
+		Pwd:  p,
+		once: sync.Once{},
+	}
+}
+
+type StaticFileAssetLoader struct {
+	once sync.Once
+	Pwd  string
+	btik map[string]string
+	db   PartsDatabase
+}
+
+func (s StaticFileAssetLoader) GetDB() PartsDatabase {
+	err := s.Load()
+	if err != nil {
+		panic(err)
+	}
+	return s.db
+}
+
+func (s StaticFileAssetLoader) Load() error {
+	var err error
+	s.once.Do(func() {
+		s.btik, err = LoadPartMap("balance_to_inv_key.json")
+		if err != nil {
+			return
+		}
+		s.db, err = LoadPartsDatabase("inventory_raw.json")
+	})
+	return err
+}
+
+func (s StaticFileAssetLoader) GetBtik() map[string]string {
+	err := s.Load()
+	if err != nil {
+		panic(err)
+	}
+	return s.btik
+}
+
+func LoadPartMap(file string) (m map[string]string, err error) {
+	bs, err := ioutil.ReadFile(file)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(bs, &m)
+	return
+}
+
+func LoadPartsDatabase(file string) (db PartsDatabase, err error) {
+	bs, err := ioutil.ReadFile(file)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(bs, &db)
+	return
+}
